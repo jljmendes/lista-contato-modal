57523e4d9bf2459ecb0d93dd597dabdd9a1bc363
jocabispo <jljmendesmanutencao@gmail.com>
2022-03-18 22:40:04 -0300

    Preparando ambiente para botões Editar e Deletar

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-		<script src="style.css"></script>
+		<link rel="stylesheet" href="style.css">
     </head>
     <body>
 		<div class="container">
@@ -18,7 +18,7 @@
 				</button>
 			</p>
 			<div class="listuser">
-			<h2>Lista de Usuários</h2>
+			<h2>Lista de Usuários Cadastrados</h2>
 			</div>
 			<span id="msg"></span>
             <span id="conteudo"></span><br><br><br>
